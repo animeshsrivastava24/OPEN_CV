ee4a0faca4d30b21b4a8aa1a79d4d395c3345913
ani <animeshsri.nith@gmail.com>
2017-06-12 09:53:23 +0530

    Add files via upload

---
+++ L18:Gradient and Edge Detection/canny.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    _, frame = cap.read()
+
+    cv2.imshow('Original',frame)
+    edges = cv2.Canny(frame,100,200)
+    cv2.imshow('Edges',edges)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
